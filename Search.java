7c337188264453031fb7ee928603b1cbe8ce10d8
photz <peterbeischmidt@gmail.com>
2014-11-30 12:02:44 +0100

    various changes

--- Search.java
+++ Search.java
@@ -1,4 +1,9 @@
+import java.util.Set;
+import java.util.Arrays;
+import java.util.Collection;
 
+
+import java.util.HashSet;
 
 public class Search {
 
@@ -14,26 +19,68 @@ public class Search {
 
 	    System.out.println("corps: " + corpusfile);
 
+	    
+
 	    Index index = new Index();
 
-	    DocumentCollection docs = 
-		new DocumentCollection(corpusfile, index);
+	    try {
+		DocumentCollection docs = 
+		    new DocumentCollection(corpusfile, index);
 
-	    index.save(indexfile);
+		index.save(indexfile);
+	    }
+	    catch (Exception e) {
+		System.out.println("sth went wrong!");
+	    }
 	}
 	// search for a phrase
 	else if (args.length == 1) {
 	    
 	    String phrase = args[0];
 
-	    Index index = new Index(indexfile);
+	    try {
+		Index index = new Index(indexfile);
 
+		Collection<Integer> res = index.findPhrase(phrase);
+
+		System.out.println("Matches: " + (String) String.valueOf(res.size()));
+
+		for (Integer docid : res) {
+		    System.out.println(String.valueOf(docid));
+		}
+		
+	    }
+	    catch (Exception e) {
+		System.out.println("sth went wrong!" + e.getMessage());
+	    }
+	    
 	}
 	// boolean search
 	else if (args.length > 1) {
 	    
-	    
+	    Set<String> termset = new HashSet<String>(Arrays.asList(args));
 
+	    try {
+		Index index = new Index(indexfile);
+
+		Collection<Integer> res = index.andSearch(termset);
+
+		System.out.println("Matches: " + (String) String.valueOf(res.size()));
+
+		for (Integer docid : res) {
+		    System.out.println(String.valueOf(docid));
+		}
+		
+	    }
+	    catch (Exception e) {
+		System.out.println("sth went wrong!" + e.getMessage());
+	    }
+
+
+	}
+	// print usage information
+	else {
+	    System.out.println("Usage: [-index <corpusfile>|<phrase>|<term1> <term2> ... <termn>]");
 	}
 
 
